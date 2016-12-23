d39d3d0ba4f028ed659ae0dd804360583bd0938e
Anderson Santos <anderson@gruponeuro.com.br>
2016-12-23 02:49:17 -0200

    Coverage CI fix

--- package/test/runInstanceTests.sh
+++ package/test/runInstanceTests.sh
@@ -7,12 +7,12 @@ rm -f /app/package/test/coverage.xml
 cd /app/package/test
 
 coverage run -m unittest discover . "*_test.py"
-#coverage run -m unittest discover . "*_standalone_mongod_test.py"
 result=$?
 
 if [[ $result == 0 ]]; then
     coverage report
     coverage xml
+    coveralls
 fi
 
 ps -aux | grep mongo | sed '/grep/D' | cut -f3 -d " " | xargs --no-run-if-empty kill -9
